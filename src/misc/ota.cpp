9023fa13fd02cac3e87c23a41d1b8d5decc9102f
Lübbe <luebbe.opensource@gmail.com>
2017-06-11 18:55:17 +0200

    Beautify OTA output on serial

--- src/misc/ota.cpp
+++ src/misc/ota.cpp
@@ -5,41 +5,48 @@
 // -----------------------------------------------------------------------------
 
 void otaSetup(uint16_t port, const char *password) {
+  unsigned int lastprogress = -1;
 
-    ArduinoOTA.setPort(port);
-    ArduinoOTA.setHostname(WiFi.hostname().c_str());    // Hostname defaults to esp8266-[ChipID]
-    
-    if (password != "") {
-      ArduinoOTA.setPassword(password);
+  ArduinoOTA.setPort(port);
+  ArduinoOTA.setHostname(WiFi.hostname().c_str());    // Hostname defaults to esp8266-[ChipID]
+
+  if (password != "") {
+    ArduinoOTA.setPassword(password);
+  }
+
+  ArduinoOTA.onStart([]() {
+    Homie.getLogger() << "[OTA] Start" << endl ;
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Homie.getLogger() << endl << "[OTA] End" << endl ;
+  });
+
+  ArduinoOTA.onProgress([lastprogress](unsigned int progress, unsigned int total) mutable {
+    unsigned int curprogress = (progress / (total / 100));
+    if (lastprogress != curprogress) {
+      lastprogress = curprogress;
+      if ((curprogress > 0) && (curprogress % 10 == 0)) {
+        Homie.getLogger() << curprogress << "%";
+      }
+      else {
+        Homie.getLogger() << ".";
+      }
     }
+  });
 
-    ArduinoOTA.onStart([]() {
-      Homie.getLogger() << "[OTA] Start" << endl ;
-    });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Homie.getLogger() << "[OTA] Error " << error << " : " << endl;
+    //DEBUG_MSG("\n[OTA] Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Homie.getLogger() << "Auth Failed" << endl;
+    else if (error == OTA_BEGIN_ERROR)Homie.getLogger() << "Begin Failed" << endl;
+    else if (error == OTA_CONNECT_ERROR) Homie.getLogger() << "Connect Failed" << endl;
+    else if (error == OTA_RECEIVE_ERROR) Homie.getLogger() << "Receive Failed" << endl;
+    else if (error == OTA_END_ERROR) Homie.getLogger() << "End Failed" << endl;
+  });
 
-    ArduinoOTA.onEnd([]() {
-      Homie.getLogger() << "[OTA] End" << endl ;
-    });
-
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-        Homie.getLogger() << "[OTA] Progress:" << (progress / (total / 100)) << "%" << endl;
-        //DEBUG_MSG("[OTA] Progress: %u%%\r", (progress / (total / 100)));
-    });
-
-    ArduinoOTA.onError([](ota_error_t error) {
-            Homie.getLogger() << "[OTA] Error " << error << " : " << endl;
-            //DEBUG_MSG("\n[OTA] Error[%u]: ", error);
-            if (error == OTA_AUTH_ERROR) Homie.getLogger() << "Auth Failed" << endl;
-            else if (error == OTA_BEGIN_ERROR)Homie.getLogger() << "Begin Failed" << endl;
-            else if (error == OTA_CONNECT_ERROR) Homie.getLogger() << "Connect Failed" << endl;
-            else if (error == OTA_RECEIVE_ERROR) Homie.getLogger() << "Receive Failed" << endl;
-            else if (error == OTA_END_ERROR) Homie.getLogger() << "End Failed" << endl;
-
-    });
-
-    ArduinoOTA.begin();
-    Homie.getLogger() << "[OTA] setup finished\n\n" << endl;
-
+  ArduinoOTA.begin();
+  Homie.getLogger() << "[OTA] setup finished\n\n" << endl;
 }
 
 void otaLoop() {
