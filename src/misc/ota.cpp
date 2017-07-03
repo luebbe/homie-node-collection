ff0ef7aab1aa7d17d744217498b8c8391dfba566
Lübbe <luebbe.opensource@gmail.com>
2017-07-03 07:51:35 +0200

    First step to wrap OTA (with display) into a class

--- src/misc/ota.cpp
+++ src/misc/ota.cpp
@@ -4,6 +4,110 @@
 // OTA
 // -----------------------------------------------------------------------------
 
+OtaWrapper::OtaWrapper() {
+
+};
+
+String OtaWrapper::getErrorMessage(ota_error_t error) {
+  if (error == OTA_AUTH_ERROR) return "Auth Failed";
+  else if (error == OTA_BEGIN_ERROR) return "Begin Failed";
+  else if (error == OTA_CONNECT_ERROR) return "Connect Failed";
+  else if (error == OTA_RECEIVE_ERROR) return "Receive Failed";
+  else if (error == OTA_END_ERROR) return "End Failed";
+};
+
+void OtaWrapper::setup(uint16_t port, const char *password) {
+  ArduinoOTA.setPort(port);
+  ArduinoOTA.setHostname(WiFi.hostname().c_str());    // Hostname defaults to esp8266-[ChipID]
+
+  if (password != "") {
+    ArduinoOTA.setPassword(password);
+  }
+
+  ArduinoOTA.onStart([this]() {
+    onStart();
+  });
+
+  ArduinoOTA.onEnd([this]() {
+    onEnd();
+  });
+
+  ArduinoOTA.onProgress([this](unsigned int progress, unsigned int total) {
+    onProgress(progress, total);
+  });
+
+  ArduinoOTA.onError([this](ota_error_t error) {
+    onError(error);
+  });
+
+  ArduinoOTA.begin();
+  Homie.getLogger() << "[OTA] setup finished\n\n" << endl;
+};
+
+void OtaWrapper::loop() {
+  ArduinoOTA.handle();
+};
+
+void OtaWrapper::onStart() {
+  Homie.getLogger() << "[OTA] Start" << endl ;
+};
+
+void OtaWrapper::onEnd() {
+  Homie.getLogger() << endl << "[OTA] End" << endl ;
+};
+
+void OtaWrapper::onProgress(unsigned int progress, unsigned int total) {
+  static int lastprogress = -1;
+  unsigned int curprogress = (progress / (total / 100));
+  if (lastprogress != curprogress) {
+    lastprogress = curprogress;
+    if (curprogress > 0) {
+      Homie.getLogger() << ".";
+      if (curprogress % 25 == 0) {
+        const char * sp;
+        if(curprogress < 100) {
+          sp = " ";
+        } else {
+          sp = "";
+        };
+        Homie.getLogger() << " [" << sp << curprogress << "% xx]" << endl;
+      }
+    }
+  }
+};
+
+void OtaWrapper::onError(ota_error_t error) {
+  Homie.getLogger() << "[OTA] Error " << getErrorMessage(error) << " : " << endl;
+};
+
+OtaDisplayWrapper::OtaDisplayWrapper(OLEDDisplay *display)
+  : OtaWrapper() {
+  _display = display;
+};
+
+void OtaDisplayWrapper::setup(uint16_t port, const char *password) {
+  Homie.getLogger() << "Setup Display";
+
+  _display->init();
+  _display->clear();
+  _display->display();
+
+  _display->flipScreenVertically();
+  OtaWrapper::setup(port, password);
+  Homie.getLogger() << " done" << endl;
+}
+
+void OtaDisplayWrapper::onProgress(unsigned int progress, unsigned int total) {
+  _display->clear();
+  _display->setTextAlignment(TEXT_ALIGN_CENTER);
+  _display->setFont(ArialMT_Plain_10);
+  _display->drawString(64, 10, "OTA Update");
+  _display->drawProgressBar(2, 28, 124, 10, progress / (total / 100));
+  _display->display();
+  OtaWrapper::onProgress(progress, total);
+};
+
+// ----------
 void otaSetup(uint16_t port, const char *password) {
   unsigned int lastprogress = -1;
 
--- src/misc/ota.hpp
+++ src/misc/ota.hpp
@@ -1,9 +1,51 @@
+/*
+ * OtaWrapper.hpp
+ * Wraps ArduinoOTA into a class and displays the OTA
+ * status on serial or an OLED display.
+ *
+ * Version: 1.0
+ * Author: Lübbe Onken (http://github.com/luebbe)
+ */
+
 #ifndef SRC_OTA_H
 #define SRC_OTA_H
 
 
-#include <Homie.h> // used for logger
+#include <Homie.h>       // used for logger output
+#include <OLEDDisplay.h> // used for display output
 #include <ArduinoOTA.h>
+
+class OtaWrapper {
+private:
+protected:
+  String getErrorMessage(ota_error_t error);
+
+public:
+  OtaWrapper();
+
+  virtual void setup(uint16_t port = 8266, const char *password = "");
+  virtual void loop();
+
+  virtual void onStart();
+  virtual void onEnd();
+  virtual void onProgress(unsigned int progress, unsigned int total);
+  virtual void onError(ota_error_t error);
+};
+
+class OtaDisplayWrapper : public OtaWrapper {
+private:
+  OLEDDisplay *_display;
+protected:
+public:
+  OtaDisplayWrapper(OLEDDisplay *display);
+
+  void setup(uint16_t port = 8266, const char *password = "") override;
+
+  void onProgress(unsigned int progress, unsigned int total) override;
+  // void setup(uint16_t port, const char *password) override;
+  // void loop() override;
+};
+
 
 void otaSetup(uint16_t port = 8266, const char *password = "");
 
