a75ad3aef1bf893bbd937c807dad74c1ffed5039
Lübbe <luebbe.opensource@gmail.com>
2017-10-10 23:56:44 +0200

    OTA: - Basic support for displays via the u8g2 library

--- src/misc/ota.hpp
+++ src/misc/ota.hpp
@@ -3,16 +3,21 @@
  * Wraps ArduinoOTA into a class and shows the OTA
  * status on serial or an OLED display.
  *
- * Version: 1.0
+ * Version: 1.1
  * Author: Lübbe Onken (http://github.com/luebbe)
  */
 
 #ifndef SRC_OTA_H
 #define SRC_OTA_H
 
-#include <Homie.h>       // used for logger output
-#include <OLEDDisplay.h> // used for display output
+#include <Homie.h>       // for Homie logger output
 #include <ArduinoOTA.h>
+#ifdef USE_SSD1306
+  #include <OLEDDisplay.h> // for display output via https://github.com/squix78/esp8266-oled-ssd1306 library
+#endif
+// #ifdef USE_U8G2
+  #include <u8g2lib.h>     // for display output via https://github.com/olikraus/u8g2 library
+// #endif
 
 // -----------------------------------------------------------------------------
 // OTA info via Homie logger
@@ -35,20 +40,40 @@ public:
 };
 
 // -----------------------------------------------------------------------------
-// OTA info via OLED Display
+// OTA info via SSD1306 128x64 OLED Display using the esp8266-oled-ssd1306 library
 // -----------------------------------------------------------------------------
-
-class OtaDisplay : public OtaLogger {
+#ifdef USE_SSD1306
+class OtaDisplaySSD1306 : public OtaLogger {
 private:
   OLEDDisplay *_display;
-  int _barTop;
 protected:
   void onEnd();
   void onProgress(unsigned int progress, unsigned int total) override;
 public:
-  OtaDisplay(OLEDDisplay *display, int height = 64);
+  OtaDisplaySSD1306(OLEDDisplay *display);
 
   void setup(uint16_t port = 8266, const char *password = "") override;
 };
+#endif
+
+// -----------------------------------------------------------------------------
+// OTA info via OLED Display using the u8g2 library
+// -----------------------------------------------------------------------------
+// #ifdef USE_U8G2
+class OtaDisplayU8G2 : public OtaLogger {
+private:
+  U8G2 *_display;
+  uint8_t _height;
+  uint8_t _width;
+protected:
+  void onStart();
+  void onEnd();
+  void onProgress(unsigned int progress, unsigned int total) override;
+public:
+  OtaDisplayU8G2(U8G2 *display);
+
+  void setup(uint16_t port = 8266, const char *password = "") override;
+};
+// #endif
 
 #endif /* end of include guard: SRC_OTA_H */
