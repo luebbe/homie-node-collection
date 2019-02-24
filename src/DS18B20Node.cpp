931a33b34bce2936961b425430e4c6b15c9f1b56
Marcus Klein <himself@kleini.org>
2019-02-24 21:30:54 +0100

    added Homie node for DS18B20

---
+++ src/DS18B20Node.cpp
@@ -0,0 +1,58 @@
+/*
+ * DS18B20Node.cpp
+ * Homie Node for Dallas 18B20 sensors.
+ *
+ * Version: 1.0
+ * Author: Marcus Klein (http://github.com/kleini)
+ */
+
+#include "DS18B20Node.hpp"
+
+DS18B20Node::DS18B20Node(const char *name, const int sensorPin, const int measurementInterval)
+    : HomieNode(name, "DS18N20Sensor"), _sensorPin(sensorPin), _measurementInterval(measurementInterval), _lastMeasurement(0) {
+  if (_sensorPin > DEFAULTPIN) {
+    oneWire = new OneWire(_sensorPin);
+    dallasTemp = new DallasTemperature(oneWire);
+  }
+}
+
+void DS18B20Node::printCaption() {
+  Homie.getLogger() << cCaption << " pin[" << _sensorPin << "]:" << endl;
+}
+
+void DS18B20Node::loop() {
+  if (_sensorPin > DEFAULTPIN) {
+    if ((millis() - _lastMeasurement >= _measurementInterval * 1000UL) || (_lastMeasurement == 0)) {
+      dallasTemp->requestTemperatures();
+      temperature = dallasTemp->getTempCByIndex(0);
+      printCaption();
+      if (DEVICE_DISCONNECTED_C == temperature) {
+        Homie.getLogger() << cIndent << "Error reading from Sensor" << endl;
+        setProperty(cStatus).send("error");
+      } else {
+        Homie.getLogger() << cIndent << "Temperature: " << temperature << " °C" << endl;
+        setProperty(cStatus).send("ok");
+        setProperty(cTemperature).send(String(temperature));
+      }
+      _lastMeasurement = millis();
+    }
+  }
+}
+
+void DS18B20Node::onReadyToOperate() {
+  setProperty(cTemperatureUnit).send("°C");
+};
+
+void DS18B20Node::setup() {
+  advertise(cStatus);
+  advertise(cTemperature);
+  advertise(cTemperatureUnit);
+
+  printCaption();
+  Homie.getLogger() << cIndent << "Reading interval: " << _measurementInterval << " s" << endl;
+
+  if (dallasTemp) {
+    dallasTemp->begin();
+    Homie.getLogger() << cIndent << "Found " << dallasTemp->getDS18Count() << " sensors." << endl;
+  }
+}
---
+++ src/DS18B20Node.hpp
@@ -0,0 +1,48 @@
+/*
+ * DS18B20Node.hpp
+ * Homie Node for Dallas 18B20 sensors.
+ *
+ * Version: 1.0
+ * Author: Marcus Klein (http://github.com/kleini)
+ */
+
+#pragma once
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#include <Homie.hpp>
+#include "constants.hpp"
+
+#define DEFAULTPIN -1
+
+class DS18B20Node : public HomieNode {
+private:
+  static const int MEASUREMENT_INTERVAL = 300;
+  const char *cCaption = "• DS18B20 sensor";
+  const char *cIndent = "  ◦ ";
+
+  int _sensorPin;
+  unsigned long _measurementInterval;
+  unsigned long _lastMeasurement;
+
+  float temperature = NAN;
+
+  OneWire *oneWire;
+  DallasTemperature *dallasTemp;
+
+  void printCaption();
+
+protected:
+  virtual void setup() override;
+  virtual void loop() override;
+
+public:
+  DS18B20Node(const char *name,
+            const int sensorPin = DEFAULTPIN,
+            const int measurementInterval = MEASUREMENT_INTERVAL);
+
+  float getTemperature() const { return temperature; }
+
+  void onReadyToOperate();
+};
