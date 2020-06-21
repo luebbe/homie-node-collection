844a0fbb2b1d1c6b2349569140b4182b9efc146d
jimtng <2554958+jimtng@users.noreply.github.com>
2020-06-22 01:00:57 +1000

    Fix call to setMicrosecondsToMeter

--- src/PingNode.cpp
+++ src/PingNode.cpp
@@ -135,7 +135,7 @@ PingNode &PingNode::setTemperature(float temperatureCelcius)
                     << "SpeedOfSound: " << soundSpeed << " " << cUnitMetersPerSecond
                     << " at " << temperatureCelcius << " " << cUnitDegrees << endl;
   // Calculating the distance from d = t_ping /2 * c => t_ping /2 * 337 [m/s] => t_ping_us / 1e-6 * 1/2 * 337
-  setMicrosecondsToMeters(0.5e-6 * soundSpeed);
+  setMicrosecondsToMeter(0.5e-6 * soundSpeed);
   return *this;
 }
 
